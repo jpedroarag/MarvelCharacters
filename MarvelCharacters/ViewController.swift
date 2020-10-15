783df8caa558f1d9254f63771502bf679223a6ec
João Pedro <jpedroarag@hotmail.com>
2020-10-15 18:26:36 -0300

    Initial Commit

---
+++ MarvelCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by João Pedro Aragão on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
